eabb7925843337b190943ea950f4e7bd436b6683
Juan Jose Araujo Bedon <juan.araujo.bedon@udla.edu.ec>
2023-10-18 09:15:32 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TallerMVC2JuanJoseAraujo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
